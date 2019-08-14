ce021f584bbd3b9c82f9b00b60ee8bd4dbb5796d
David Mulder <dmulder@suse.com>
2019-08-14 14:13:07 -0600

    Initial commit

---
+++ admin-tools.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from yast import import_module
+import_module('UI')
+from yast import *
+from subprocess import Popen
+
+header = Header('Name')
+items = [Item(Id('aduc'), 'Active Directory Users and Computers'),
+         Item(Id('adsi'), 'ADSI Edit'),
+         Item(Id('dns-manager'), 'DNS'),
+         Item(Id('gpmc'), 'Group Policy Management Console'),
+]
+UI.OpenDialog(Opt('mainDialog'), Table(Id('tools'), Opt('notify'), header, items))
+UI.SetApplicationTitle('Administrative Tools')
+
+UI.WaitForEvent()
+module = UI.QueryWidget('tools', 'Value')
+UI.CloseDialog()
+
+Popen(['y2base', module, 'ncurses']).wait()
