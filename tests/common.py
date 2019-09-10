6c01203da107aea692dc52f36c38c32eacd74835
David Mulder <dmulder@suse.com>
2019-09-10 13:26:53 -0600

    Try to load the SamDB

--- tests/common.py
+++ tests/common.py
@@ -1,10 +1,14 @@
 import unittest
 import hecate
-from adcommon.creds import kinit_for_gssapi
+from adcommon.creds import kinit_for_gssapi, pdc_dns_name
 from samba.credentials import Credentials, MUST_USE_KERBEROS
+from samba.param import LoadParm
+from samba.auth import system_session
+from samba.samdb import SamDB
 from getpass import getpass
 from subprocess import Popen, PIPE
 import re, six
+import ldapurl
 
 class AdminToolsTestCase(unittest.TestCase):
     def assertSeen(self, what, msg=None, timeout=10):
@@ -19,15 +23,19 @@ class AdminToolsTestCase(unittest.TestCase):
 
     def __validate_kinit(self):
         out, _ = Popen(['klist'], stdout=PIPE, stderr=PIPE).communicate()
+        m = re.findall(six.b('Ticket cache:\s*(.*)'), out)
+        if len(m) != 1:
+            return None
+        self.creds.set_named_ccache(m[0].decode())
         m = re.findall(six.b('Default principal:\s*(\w+)@([\w\.]+)'), out)
         if len(m) == 0:
             return False
         user, realm = m[0]
         if Popen(['klist', '-s'], stdout=PIPE, stderr=PIPE).wait() != 0:
             return False
+        self.creds.set_kerberos_state(MUST_USE_KERBEROS)
         self.creds.set_username(user.decode())
         self.creds.set_domain(realm.decode())
-        self.creds.set_kerberos_state(MUST_USE_KERBEROS)
         return True
 
     def kinit(self):
@@ -39,4 +47,11 @@ class AdminToolsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.creds = Credentials()
+        self.lp = LoadParm()
+        self.lp.load_default()
+        self.creds.guess(self.lp)
         self.kinit()
+        self.lp.set('realm', self.creds.get_domain())
+        ldap_url = ldapurl.LDAPUrl('ldap://%s' % pdc_dns_name(self.creds.get_domain()))
+        self.samdb = SamDB(url=ldap_url.initializeUrl(), session_info=system_session(),
+                           credentials=self.creds, lp=self.lp)
