175e0e90423b70331b3163971fd37da7720642c7
David Mulder <dmulder@suse.com>
2019-09-23 22:30:58 +0000

    Dummy yast module for non-yast clients

---
+++ tests/yast.py
@@ -0,0 +1,17 @@
+class ycpbuiltins:
+    def y2error(msg):
+        print(msg)
+
+def import_module(module):
+    pass
+
+class Declare:
+    def __init__(self, *args):
+        pass
+
+    def __call__(self, func):
+        return func
+
+class UI:
+    def __init__(self, *args):
+        pass
