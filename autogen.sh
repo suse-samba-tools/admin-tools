3ca3ffa4b59e7abe377074c3df061d2697e4077a
David Mulder <dmulder@suse.com>
2020-05-01 09:45:38 -0600

    Add an autogen script to clean up some autoreconf issues

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ac_abs_confdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+for i in NEWS README AUTHORS ChangeLog; do
+	if [ ! -f ${ac_abs_confdir}/${i} ] ; then
+		touch ${ac_abs_confdir}/${i}
+	fi
+done
+sed 's;@TOP_SRiC_DIR@;'${ac_abs_confdir}';g' ${ac_abs_confdir}/src/yast-ruby-bindings.make.in > ${ac_abs_confdir}/src/yast-ruby-bindings/Makefile.am && autoreconf -if
