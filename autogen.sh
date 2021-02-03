03e3cee85882507904337ab756e50c21803a140e
David Mulder <dmulder@suse.com>
2021-02-03 13:36:16 -0700

    Fix ncurses configure failure on Debian

--- autogen.sh
+++ autogen.sh
@@ -6,4 +6,5 @@ for i in NEWS README AUTHORS ChangeLog; do
 		touch ${ac_abs_confdir}/${i}
 	fi
 done
-sed 's;@TOP_SRiC_DIR@;'${ac_abs_confdir}';g' ${ac_abs_confdir}/src/yast-ruby-bindings.make.in > ${ac_abs_confdir}/src/yast-ruby-bindings/Makefile.am && autoreconf -if
+patch -d ${ac_abs_confdir}/src/libyui-ncurses -p1 < ${ac_abs_confdir}/src/find_curses.patch
+sed 's;@TOP_SRC_DIR@;'${ac_abs_confdir}';g' ${ac_abs_confdir}/src/yast-ruby-bindings.make.in > ${ac_abs_confdir}/src/yast-ruby-bindings/Makefile.am && autoreconf -if
